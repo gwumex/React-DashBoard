15f3658857c1313cd5a75eaff0245648f88d9595
gwumex <42099116+gwumex@users.noreply.github.com>
2022-12-19 19:41:11 +0100

    employees page completed

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Toolbar, Inject} from '@syncfusion/ej2-react-grids'
 
-const Employee = () => {
+import {employeesData, contextMenuItems, employeesGrid} from '../data/dummy';
+import { Header } from '../components';
+import { Grid } from '@syncfusion/ej2-react-grids';
+
+const Employees = () => {
   return (
-    <div>Employee</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource = {employeesData}
+        toolbar={['Search']}
+        allowPaging
+        allowSorting
+        width="auto">
+        <ColumnsDirective>{employeesGrid.map((item, index) => {
+          return(
+              <ColumnDirective key={index} {...item}/>
+          )
+        })}</ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]}/>
+      </GridComponent>
+    </div>
   )
 }
 
-export default Employee
+export default Employees

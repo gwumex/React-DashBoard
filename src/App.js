6cd73a9c192ee68b3d42b0a08688fcc6cc372d48
gwumex <42099116+gwumex@users.noreply.github.com>
2022-12-19 19:31:23 +0100

    orders page completed

--- src/App.js
+++ src/App.js
@@ -25,11 +25,11 @@ const App = () => {
           </div>
           {
             activeMenu ? (
-              <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white z-40'>
                 <Sidebar />
               </div>
             ) : (
-              <div className='w-0 dark:bg-secondary-dark-bg'>
+              <div className='w-0 dark:bg-secondary-dark-bg '>
                 <Sidebar/>
               </div>
             )

6175b2c168cb09011296f2a6a1c0b4ba5d278ab7
gwumex <42099116+gwumex@users.noreply.github.com>
2022-12-20 11:16:40 +0100

    themesettings and dark mode completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import React, { useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages'
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
 
 import { useStateContext } from './contexts/ContextProvider';
-
 
 const App = () => {
   const { activeMenu } = useStateContext();
